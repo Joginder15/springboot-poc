f6f7c95cd19b95ee53e9d406bc2173335a2ffd2a
joginder.kumar <joginder.kumar@cogneesol.net>
2025-08-23 17:36:28 +0530

    logging is done

--- src/main/java/com/engineering/journalApp/service/JournalEntryService.java
+++ src/main/java/com/engineering/journalApp/service/JournalEntryService.java
@@ -4,6 +4,8 @@ import com.engineering.journalApp.entity.JournalEntry;
 import com.engineering.journalApp.entity.User;
 import com.engineering.journalApp.repository.JournalEntryRepository;
 import org.bson.types.ObjectId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
--- src/main/java/com/engineering/journalApp/service/UserService.java
+++ src/main/java/com/engineering/journalApp/service/UserService.java
@@ -1,21 +1,22 @@
 package com.engineering.journalApp.service;
 
-import com.engineering.journalApp.entity.JournalEntry;
 import com.engineering.journalApp.entity.User;
 import com.engineering.journalApp.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.bson.types.ObjectId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 
 @Component
+@Slf4j
 public class UserService {
 
     @Autowired
@@ -23,12 +24,19 @@ public class UserService {
 
     private static final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
+    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
+
     public User saveNewUser(User user){
         try {
             user.setPassword(passwordEncoder.encode(user.getPassword()));
             user.setRoles(Arrays.asList("USER"));
             return userRepository.save(user);
         } catch (Exception e){
+            log.error("dsvvsvsvs");
+            log.warn("saadsfaefa");
+            log.info("saadsfaefa");
+            log.debug("saadsfaefa");
+            log.trace("saadsfaefa");
             throw new RuntimeException("Not found");
         }
     }
