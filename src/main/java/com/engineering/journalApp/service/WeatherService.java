e701a6a7c1a24726439228aab8cec9a796a92b9f
joginder.kumar <joginder.kumar@cogneesol.net>
2025-08-30 18:05:06 +0530

    Value annotation is used

--- src/main/java/com/engineering/journalApp/service/WeatherService.java
+++ src/main/java/com/engineering/journalApp/service/WeatherService.java
@@ -2,6 +2,7 @@ package com.engineering.journalApp.service;
 
 import com.engineering.journalApp.dto.WeatherResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -10,7 +11,9 @@ import org.springframework.web.client.RestTemplate;
 
 @Service
 public class WeatherService {
-    private static final String API_KEY = "aa64476ba28de3e85749789ad34830cd";
+
+    @Value("${weather.api.key}")
+    private String API_KEY;
 
     private static final String API = "http://api.weatherstack.com/current?access_key=API_KEY&query=CITY";
 
